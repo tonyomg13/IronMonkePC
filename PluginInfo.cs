c64444cde03580f596d429c4e9ba8301502e7b59
BzzzThe18th <69125495+BzzzThe18th@users.noreply.github.com>
2022-02-09 20:24:19 -0800

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace IronMonke
+{
+    class PluginInfo
+    {
+        public const string GUID = "com.buzzbzzzbzzbzzzthe18th.gorillatag.ironmonkepc";
+        public const string Name = "IronMonke";
+        public const string Version = "1.0.0";
+    }
+}
